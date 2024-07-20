b4bb4ebff1261068e82a356ef476a91e155fa593
= <=>
2024-07-20 14:38:47 +0530

    added

--- FRONTEND/ParkingLotManagement/src/components/Header2/Components/Navbar/Navbar.jsx
+++ FRONTEND/ParkingLotManagement/src/components/Header2/Components/Navbar/Navbar.jsx
@@ -33,17 +33,17 @@ const Navbar = () => {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/About" className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`}>
+                                <NavLink to="/About" className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-white"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`}>
                                     About
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/Contact" className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`}>
+                                <NavLink to="/Contact" className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-white"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`}>
                                     Contact
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/ParkingView" className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`}>
+                                <NavLink to="/ParkingView" className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-white"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`}>
                                     Parking View
                                 </NavLink>
                             </li>
--- FRONTEND/ParkingLotManagement/src/main.jsx
+++ FRONTEND/ParkingLotManagement/src/main.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import App from './App.jsx'
 import './index.css'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter,createRoutesFromElements} from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
-import Contact from './components/Contact/Contact.jsx' 
+import Contact from './components/Contact/Contact.jsx'
 import CustomerForm from './components/SmallReusableComponents/CustomerForm.jsx'
 import LogIn from './components/SmallReusableComponents/LogIn.jsx'
 import VehicleForm from './components/SmallReusableComponents/VehicleForm .jsx'
@@ -17,51 +17,55 @@ import VehicleForm from './components/SmallReusableComponents/VehicleForm .jsx'
 import ParkingView from './components/ParkingView/ParkingView.jsx'
 import Header2 from './components/Header2/Header.jsx'
 
-const router =createBrowserRouter([
+import { Provider } from "react-redux"
+import store from "./store/store.js"
+const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Layout/>,
-    children:[
+    path: "/",
+    element: <Layout />,
+    children: [
       {
-        path:"",
-        element:<Header2/>
+        path: "",
+        element: <Header2 />
       },
-      
+
       {
-        path:"About",
-        element:<About/>
-      },
-      {
-        path:"Contact",
-        element:<Contact/>
-      }, 
-      {
-        path:"logIn",
-        element:<LogIn/>
-      },
-      
-      {
-        path:"Register2",
-        element:<VehicleForm/>
+        path: "About",
+        element: <About />
       },
       {
-        path:"register",
-        element:<CustomerForm/>
+        path: "Contact",
+        element: <Contact />
       },
       {
-        path:"ParkingView",
-        element:<ParkingView/>
+        path: "logIn",
+        element: <LogIn />
+      },
+
+      {
+        path: "Register2",
+        element: <VehicleForm />
+      },
+      {
+        path: "register",
+        element: <CustomerForm />
+      },
+      {
+        path: "ParkingView",
+        element: <ParkingView />
       }
     ],
-    
+
   },
   {
-    path:"/Home",
-    element:<Header2/>
+    path: "/Home",
+    element: <Header2 />
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
