169122b184769c5588f5205572809602bd132ceb
= <=>
2024-07-20 17:50:05 +0530

    commit frontend worked

--- FRONTEND/ParkingLotManagement/src/components/Dashboard/Dashboard.jsx
+++ FRONTEND/ParkingLotManagement/src/components/Dashboard/Dashboard.jsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
                         backgroundRepeat: 'no-repeat'
                     }}
                 >
-                    <Background playStatus={playStatus} heroCount={heroCount} />
+                    {/* <Background playStatus={playStatus} heroCount={heroCount} /> */}
                     <div className="hero-overlay bg-opacity-60"></div>
                     <div className="hero-content text-neutral-content text-center">
                         <div className="max-w-md">
--- FRONTEND/ParkingLotManagement/src/index.js
+++ FRONTEND/ParkingLotManagement/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import About from "./components/About/About";
 import CustomerForm from "./components/Auth/CustomerForm";
@@ -24,29 +23,3 @@ export {
   VehicleForm,
 };
 
-=======
-import About from "./components/About/About.jsx";
-import CustomerForm from "./components/Auth/CustomerForm.jsx";
-import LogIn from "./components/Auth/LogIn.jsx";
-import Contact from "./components/Contact/Contact.jsx";
-import Dashboard from "./components/Dashboard/Dashboard.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import Header from "./components/Header/Header.jsx";
-import ParkingView from "./components/ParkingView/ParkingView.jsx";
-import Reservations from "./components/Reservations/Reservations.jsx";
-import VehicleForm from "./components/VehicleForm/VehicleForm .jsx"
-
-export {
-    About,
-    CustomerForm,
-    LogIn,
-    Contact,
-    Dashboard,
-    Footer,
-    Header,
-    ParkingView,
-    Reservations,
-    VehicleForm
-  };
-  
->>>>>>> 1b1178051c0a8cc50d96d9969b01b03dc47a5349
